2c5228ae706570ae0378ac6ee0c2a6aecb9e121d
Monica Powell <monica@aboutmonica.com>
2020-08-16 17:54:45 -0400

    initial commit with rough content

---
+++ theme.js
@@ -0,0 +1,18 @@
+const eggplant = "#2a2135";
+const white = "#fbf4ff";
+export default {
+  fonts: {
+    body: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+  },
+  colors: {
+    text: eggplant,
+    background: white,
+    primary: "yellow",
+  },
+  pre: {
+    width: "50px",
+  },
+  code: {
+    width: "100px",
+  },
+};
