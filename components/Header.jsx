995c3987165e75d15cad469fc82bb0aada29706a
monicafresh <monica@aboutmonica.com>
2020-08-19 09:28:28 -0400

    add link to slides and update slide content

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 export default function Header({ name, title, date, url }) {
   return (
@@ -7,13 +7,13 @@ export default function Header({ name, title, date, url }) {
       <div>
         <a href={url}>
           <span>{name}</span>
-        </a>{' '}
-        —{' '}
+        </a>{" "}
+        —{" "}
         <Link href="/1">
           <a>{title}</a>
         </Link>
       </div>
-      <time>{date}</time>
+      <a href={`https://www.${date}`}>{date}</a>
     </header>
-  )
+  );
 }
--- site.config.js
+++ site.config.js
@@ -1,10 +1,10 @@
 export const siteConfig = {
   name: `@waterproofheart`,
   title: `Migrating to MDX Deck`,
-  date: `August 2020`,
+  date: `monica.dev/migratingtomdx`,
   author: {
     url: `https://twitter.com/waterproofheart`,
   },
-}
+};
 
-export default siteConfig
+export default siteConfig;
